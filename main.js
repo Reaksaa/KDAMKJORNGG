c6a9d2eae50898ff1eaa6a5fab5501dae41de4cc
Reaksa Asa <oystersus54@gmail.com>
2025-12-23 23:43:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const modalTitle = document.querySelector("#movieModal .modal-title");
   const modalVideo = document.querySelector("#movieModal iframe");
 
+  // Movie array with YouTube IDs
   const movies = [
-    { title: "Batman", rating: "⭐️ 9.0/10", img: "https://drive.google.com/uc?export=view&id=1VwL8-s5erE0xnTWl4BgDqLjLISiIgd8E", videoId: "dQw4w9WgXcQ" },
-    { title: "Chainsaw Man S2", rating: "⭐️ 8.8/10", img: "https://drive.google.com/uc?export=view&id=1aQVcjKeHFxw2PDifJSb2hUowYP6IkFfR",videoId: "dQw4w9WgXcQ" },
-    { title: "Jujutsu Kaisen", rating: "⭐️ 8.6/10", img: "https://drive.google.com/uc?export=view&id=1Mh0JWXrX9LbFr3S5FJ_COAoIJIRIfrSP",videoId: "dQw4w9WgXcQ" },
+    { title: "Batman", rating: "⭐️ 9.0/10", img: "https://ntvb.tmsimg.com/assets/p8757074_v_h10_ah.jpg?w=960&h=540", videoId: "GokKUqLcvD8" },
+    { title: "Chainsaw Man S2", rating: "⭐️ 9.8/10", img: "https://static0.srcdn.com/wordpress/wp-content/uploads/2025/12/chainsaw-man-assassins-arc-poster-2.png?w=1600&h=900&fit=crop", videoId: "_PRaywfa1LE" },
+    { title: "Jujutsu Kaisen", rating: "⭐️ 8.6/10", img: "https://de7i3bh7bgh0d.cloudfront.net/2025/10/31/17/59/32/02723005-ae95-4654-9e6b-9720880310e1/JJK_v28_Social_Blog_1200x630.jpg", videoId: "MPfZhgLiK6w" },
+    // Add more movies here
   ];
 
   function renderMovies(list) {
@@ -42,6 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   renderMovies(movies);
 
+  // SEARCH
   if (searchBtn && searchInput) {
     searchBtn.addEventListener("click", () => {
       const query = searchInput.value.toLowerCase();
@@ -55,8 +58,3 @@ document.addEventListener("DOMContentLoaded", () => {
     modalVideo.src = "";
   });
 });
-
-
-
-
-
