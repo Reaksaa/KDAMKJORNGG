e17df562a758f05576f311dd618c9d5edab19558
Reaksa Asa <oystersus54@gmail.com>
2025-12-23 21:32:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const modalVideo = document.querySelector("#movieModal iframe");
 
   const movies = [
-    { title: "Batman", rating: "⭐️ 9.0/10", img: "https://drive.google.com/file/d/1VwL8-s5erE0xnTWl4BgDqLjLISiIgd8E/view?usp=sharing" },
-    { title: "Chainsaw Man S2", rating: "⭐️ 8.8/10", img: "https://drive.google.com/file/d/1aQVcjKeHFxw2PDifJSb2hUowYP6IkFfR/view?usp=drive_link" },
-    { title: "Jujutsu Kaisen", rating: "⭐️ 8.6/10", img: "https://drive.google.com/file/d/1Mh0JWXrX9LbFr3S5FJ_COAoIJIRIfrSP/view?usp=drive_link" },
+    { title: "Batman", rating: "⭐️ 9.0/10", img: "https://drive.google.com/uc?export=view&id=1VwL8-s5erE0xnTWl4BgDqLjLISiIgd8E" },
+    { title: "Chainsaw Man S2", rating: "⭐️ 8.8/10", img: "https://drive.google.com/uc?export=view&id=1aQVcjKeHFxw2PDifJSb2hUowYP6IkFfR" },
+    { title: "Jujutsu Kaisen", rating: "⭐️ 8.6/10", img: "https://drive.google.com/uc?export=view&id=1Mh0JWXrX9LbFr3S5FJ_COAoIJIRIfrSP" },
   ];
 
   function renderMovies(list) {
@@ -55,6 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
     modalVideo.src = "";
   });
 });
+
 
 
 
